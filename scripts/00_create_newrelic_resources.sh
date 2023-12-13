5e6714466c9cfd1d0a1479f3437bafecd82d326a
uturkarslan <ugurturkarslan@gmail.com>
2023-12-13 12:23:40 +0100

    Add deployment script

---
+++ scripts/00_create_newrelic_resources.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Get commandline arguments
+while (( "$#" )); do
+  case "$1" in
+    --destroy)
+      flagDestroy="true"
+      shift
+      ;;
+    --dry-run)
+      flagDryRun="true"
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+### Set variables
+
+if [[ $flagDestroy != "true" ]]; then
+
+  # Initialize Terraform
+  terraform -chdir=../terraform init
+
+  # Plan Terraform
+  terraform -chdir=../terraform plan \
+    -var NEW_RELIC_ACCOUNT_ID=$NEWRELIC_ACCOUNT_ID \
+    -var NEW_RELIC_API_KEY=$NEWRELIC_API_KEY \
+    -var NEW_RELIC_REGION=$NEWRELIC_REGION \
+    -out "./tfplan"
+
+  # Apply Terraform
+  if [[ $flagDryRun != "true" ]]; then
+    terraform -chdir=../terraform apply tfplan
+  fi
+else
+
+  # Destroy Terraform
+  terraform -chdir=../terraform destroy \
+    -var NEW_RELIC_ACCOUNT_ID=$NEWRELIC_ACCOUNT_ID \
+    -var NEW_RELIC_API_KEY=$NEWRELIC_API_KEY \
+    -var NEW_RELIC_REGION=$NEWRELIC_REGION
+fi
